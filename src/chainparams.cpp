ec03d92c55933de5c86704992dbc65033f61368a
Phuoc Do <dnprock@gmail.com>
2019-02-28 23:22:23 -0800

    Add seed servers

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -122,7 +122,7 @@ public:
         // This is fine at runtime as we'll fall back to using them as a oneshot if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-        //vSeeds.emplace_back("seed.bitflate.org");
+        vSeeds.emplace_back("seed.bitflate.org");
         //vSeeds.emplace_back("seed.bitcoin.sipa.be"); // Pieter Wuille, only supports x1, x5, x9, and xd
         //vSeeds.emplace_back("dnsseed.bluematt.me"); // Matt Corallo, only supports x9
         //vSeeds.emplace_back("dnsseed.bitcoin.dashjr.org"); // Luke Dashjr
@@ -234,6 +234,7 @@ public:
         vFixedSeeds.clear();
         vSeeds.clear();
         // nodes with support for servicebits filtering should be at the top
+        vSeeds.emplace_back("testnet-seed.bitflate.org");
         //vSeeds.emplace_back("testnet-seed.bitcoin.jonasschnelli.ch");
         //vSeeds.emplace_back("seed.tbtc.petertodd.org");
         //vSeeds.emplace_back("seed.testnet.bitcoin.sprovoost.nl");
