2039122e256bbeb7f6c5140213b093b564467cd3
Phuoc Do <dnprock@gmail.com>
2019-04-09 22:02:30 -0700

    Change data directory name

--- src/util/system.cpp
+++ src/util/system.cpp
@@ -687,7 +687,7 @@ fs::path GetDefaultDataDir()
     // Unix: ~/.bitcoin
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Bitcoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "Bitflate";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -697,10 +697,10 @@ fs::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/Bitcoin";
+    return pathRet / "Library/Application Support/Bitflate";
 #else
     // Unix
-    return pathRet / ".bitcoin";
+    return pathRet / ".bitflate";
 #endif
 #endif
 }
