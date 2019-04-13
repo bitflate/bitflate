4aa390a62920aaa6d515fe857db1cd4830f94f33
Phuoc Do <dnprock@gmail.com>
2019-04-13 14:43:25 -0700

    Change bitflate sticker display BFL

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -42,9 +42,9 @@ QString BitcoinUnits::longName(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("BTC");
-    case mBTC: return QString("mBTC");
-    case uBTC: return QString::fromUtf8("µBTC (bits)");
+    case BTC: return QString("BFL");
+    case mBTC: return QString("mBFL");
+    case uBTC: return QString::fromUtf8("µBFL (bits)");
     case SAT: return QString("Satoshi (sat)");
     default: return QString("???");
     }
